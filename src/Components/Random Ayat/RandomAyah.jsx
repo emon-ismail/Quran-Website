d7834b6b5dca32c1366f9f576ac7a1fcb4f12cc9
Tonmoy-Roy <emonismail69@gmail.com>
2023-08-19 12:43:30 +0600

    random ayah fixed

--- src/Components/Random Ayat/RandomAyah.jsx
+++ src/Components/Random Ayat/RandomAyah.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const RandomAyah = () => {
   const [arabicAyah, setArabicAyah] = useState('');
@@ -7,10 +7,11 @@ const RandomAyah = () => {
   const [ayahNum, setAyahNum] = useState('');
   const [audioSrc, setAudioSrc] = useState('');
 
+  const generateRandomNum = () => {
+    return Math.floor(Math.random() * 6236);
+  };
+
   useEffect(() => {
-    const generateRandomNum = () => {
-      return Math.floor(Math.random() * 6236);
-    };
     const randomNum = generateRandomNum();
 
     const arabicApi = `https://api.alquran.cloud/v1/ayah/${randomNum}/ar.alafasy`;
@@ -38,10 +39,39 @@ const RandomAyah = () => {
   };
 
   const handleGenerate = () => {
-    window.location.reload();
+    // Clear previous content
+    setArabicAyah('');
+    setBanglaAyah('');
+    setSuraName('');
+    setAyahNum('');
+    setAudioSrc('');
+
+    // Fetch new random ayah content
+    const randomNum = generateRandomNum();
+    const arabicApi = `https://api.alquran.cloud/v1/ayah/${randomNum}/ar.alafasy`;
+    const banglaApi = `https://api.alquran.cloud/v1/ayah/${randomNum}/bn.bengali`;
+
+    fetch(arabicApi)
+      .then(response => response.json())
+      .then(data => {
+        setArabicAyah(data.data.text);
+        setSuraName(data.data.surah.englishName);
+        setAyahNum(data.data.numberInSurah);
+        setAudioSrc(data.data.audio);
+      });
+
+    fetch(banglaApi)
+      .then(response => response.json())
+      .then(data => {
+        setBanglaAyah(data.data.text);
+      });
+
+    // Scroll to the newly generated content
+    const generatedContent = document.getElementById('generated-content');
+    if (generatedContent) {
+      generatedContent.scrollIntoView({ behavior: 'smooth' });
+    }
   };
-
-
 
   const generateWhatsAppUrl = (arabicText, banglaText, surahInfo) => {
     const encodedArabicText = encodeURIComponent(arabicText);
@@ -51,39 +81,35 @@ const RandomAyah = () => {
   };
 
   return (
-    <main className="text-center p-4 mt-1 ">
-   
-     <div className='bg-[#23304B]'>
-     <div className="mt-8 ">
-     <h1 className='text-green-400 py-8 pb-4 text-xl md:font-semibold'>Random Ayah of the Day</h1>
-     <h1 className=' border-b border-green-400 border-w-40 mt-2 mb-8 '></h1>
-
-        <h2 className="text-xl arabic-ayah py-8 font-al-qalam  font-normal font-weight-300 tracking-wide md:text-2xl leading-loose text-white">{arabicAyah}</h2>
-        <h3 className="text-lg bangla-ayah py-4 text-white">{banglaAyah}</h3>
-        <p className="text-green-400 sura-info py-4">
-          আল কুরআন • সুরা - {suraName} • আয়াত - {ayahNum}
-        </p>
-      </div>
-      <div className="mb-4">
-        <audio src={audioSrc}></audio>
-        <button onClick={handlePlayAudio} className="btn-play bg-blue-500 text-white px-4 py-2 rounded mr-2">
-          <i className="fas fa-play mr-1"></i> Play
+    <main className="text-center p-4 mt-1" id="generated-content">
+      <div className='bg-[#23304B]'>
+        <div className="mt-8">
+          <h1 className='text-green-400 py-8 pb-4 text-xl md:font-semibold'>Random Ayah of the Day</h1>
+          <h1 className=' border-b border-green-400 border-w-40 mt-2 mb-8 '></h1>
+          <h2 className="text-xl arabic-ayah py-8 font-al-qalam  font-normal font-weight-300 tracking-wide md:text-2xl leading-loose text-white">{arabicAyah}</h2>
+          <h3 className="text-lg bangla-ayah py-4 text-white">{banglaAyah}</h3>
+          <p className="text-green-400 sura-info py-4">
+            আল কুরআন • সুরা - {suraName} • আয়াত - {ayahNum}
+          </p>
+        </div>
+        <div className="mb-4">
+          <audio src={audioSrc}></audio>
+          <button onClick={handlePlayAudio} className="btn-play bg-blue-500 text-white px-4 py-2 rounded mr-2">
+            <i className="fas fa-play mr-1"></i> Play
+          </button>
+          <a
+            href={generateWhatsAppUrl(arabicAyah, banglaAyah, `${suraName} • আয়াত - ${ayahNum}`)}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="whatsapp-share bg-green-500 text-white px-4 py-2 rounded"
+          >
+            <i className="fab fa-whatsapp mr-1"></i> Share on WhatsApp
+          </a>
+        </div>
+        <button onClick={handleGenerate} className="btn-generate bg-gray-500 text-white px-4 py-2 mb-8 rounded">
+          <i className="fas fa-cog mr-1"></i> Generate
         </button>
-     
-      
-        <a
-  href={generateWhatsAppUrl(arabicAyah, banglaAyah, `${suraName} • আয়াত - ${ayahNum}`)}
-  target="_blank"
-  rel="noopener noreferrer"
-  className="whatsapp-share bg-green-500 text-white px-4 py-2 rounded"
->
-  <i className="fab fa-whatsapp mr-1"></i> Share on WhatsApp
-</a>
       </div>
-      <button onClick={handleGenerate} className="btn-generate bg-gray-500 text-white px-4 py-2 mb-8 rounded">
-        <i className="fas fa-cog mr-1"></i> Generate
-      </button>
-     </div>
     </main>
   );
 };
